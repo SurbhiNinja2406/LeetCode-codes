434ef70e3aedc852be2bc82ed3eff28c905b71fd
SurbhiNinja2406 <itssurbhi7@gmail.com>
2026-08-24 16:36:34 +0100

    Time: 6 ms (16.06%) | Memory: 12.3 MB (96.89%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -23,7 +23,6 @@ if __name__ == "__main__":
     print(sol.isValid("()")) 
     print(sol.isValid("()[]{}"))
     print(sol.isValid("(]")) 
-    print(sol.isValid("([])")) 
     print(sol.isValid("([)]"))  
     print(sol.isValid("("))  
     print(sol.isValid(")")) 
