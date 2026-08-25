8dbd0f67faa1e4c8964ba557b94e99d0ff645e00
SurbhiNinja2406 <itssurbhi7@gmail.com>
2026-08-25 14:10:34 +0100

    Time: 5826 ms (51.48%) | Memory: 12.3 MB (87.64%) - LeetSync

---
+++ 79-word-search/word-search.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+        if not board or not board[0]:
+            return False
+        rows, cols = len(board), len(board[0])
+        def backtrack(r, c, index):
+            if index == len(word):
+                return True
+            if r < 0 or r >= rows or c < 0 or c >= cols or board[r][c] != word[index]:
+                return False
+            temp = board[r][c]
+            board[r][c] = '#'
+            found = (backtrack(r + 1, c, index + 1) or
+                     backtrack(r - 1, c, index + 1) or
+                     backtrack(r, c + 1, index + 1) or
+                     backtrack(r, c - 1, index + 1))
+            board[r][c] = temp
+            return found
+        for row in range(rows):
+            for col in range(cols):
+                if backtrack(row, col, 0):
+                    return True
+        return False
+if __name__ == "__main__":
+    sol = Solution()
+    board1 = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
+    print(sol.exist(board1, "ABCCED"))  
+    board2 = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
+    print(sol.exist(board2, "SEE"))  
+    board3 = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
+    print(sol.exist(board3, "ABCB"))
