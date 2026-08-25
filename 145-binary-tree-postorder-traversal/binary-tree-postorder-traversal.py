0946c8a1509c7fd56b3e6ea83403b3bc3d7b57e9
SurbhiNinja2406 <itssurbhi7@gmail.com>
2026-08-25 14:57:35 +0100

    Time: 0 ms (100.00%) | Memory: 12.5 MB (1.19%) - LeetSync

---
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
@@ -0,0 +1,51 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        result = []        
+        def dfs(node):
+            if not node:
+                return            
+            dfs(node.left)           
+            dfs(node.right)           
+            result.append(node.val)   
+        dfs(root)
+        return result
+def build_tree(values):
+    if not values or values[0] is None:
+        return None    
+    root = TreeNode(values[0])
+    queue = [root]
+    i = 1    
+    while queue and i < len(values):
+        node = queue.pop(0)        
+        if i < len(values):
+            if values[i] is not None:
+                node.left = TreeNode(values[i])
+                queue.append(node.left)
+            i += 1        
+        if i < len(values):
+            if values[i] is not None:
+                node.right = TreeNode(values[i])
+                queue.append(node.right)
+            i += 1    
+    return root
+if __name__ == "__main__":
+    sol = Solution()
+    root1 = build_tree([1, None, 2, 3])
+    print(sol.postorderTraversal(root1))  
+    root2 = build_tree([1, 2, 3, 4, 5, None, 8, None, None, 6, 7, 9])
+    print(sol.postorderTraversal(root2))  
+    root3 = build_tree([])
+    print(sol.postorderTraversal(root3))  
+    root4 = build_tree([1])
+    print(sol.postorderTraversal(root4)) 
+print(__name__)
