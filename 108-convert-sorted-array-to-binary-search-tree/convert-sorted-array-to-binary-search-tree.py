de87ffa1fd98b789ef8c917c51bdfd6494c01252
SurbhiNinja2406 <itssurbhi7@gmail.com>
2026-08-25 14:37:47 +0100

    Time: 3 ms (98.66%) | Memory: 15.2 MB (43.78%) - LeetSync

---
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.py
@@ -0,0 +1,44 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: Optional[TreeNode]
+        """
+        def build(left, right):
+            if left > right:
+                return None
+            mid = (left + right) // 2
+            root = TreeNode(nums[mid])
+            root.left = build(left, mid - 1)
+            root.right = build(mid + 1, right)
+            return root
+        return build(0, len(nums) - 1)
+def tree_to_list(root):
+    if not root:
+        return []
+    result = []
+    queue = [root]
+    while queue:
+        node = queue.pop(0)
+        if node:
+            result.append(node.val)
+            queue.append(node.left)
+            queue.append(node.right)
+        else:
+            result.append(None)
+    while result and result[-1] is None:
+        result.pop()
+    return result
+if __name__ == "__main__":
+    sol = Solution()
+    root1 = sol.sortedArrayToBST([-10, -3, 0, 5, 9])
+    print("Test 1: {}".format(tree_to_list(root1)))
+    root2 = sol.sortedArrayToBST([1, 3])
+    print("Test 2: {}".format(tree_to_list(root2)))
+print(__name__)
